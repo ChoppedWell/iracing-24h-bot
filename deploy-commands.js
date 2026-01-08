06021c5775df1c009294e289f95e2fb13493f7f0
ChoppedWell <47302127+ChoppedWell@users.noreply.github.com>
2026-01-08 14:25:06 -0500

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,36 @@
+import { REST, Routes } from 'discord.js';
+import fs from 'fs';
+import config from './config.json' assert { type: 'json' };
+
+const TOKEN = process.env.DISCORD_TOKEN;
+const CLIENT_ID = '1458893923396354134';
+const GUILD_ID = '1458882296118644910';
+
+const commands = [];
+
+// Load all command files
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = await import(`./commands/${file}`);
+  commands.push(command.default.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+async function deploy() {
+  try {
+    console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands },
+    );
+
+    console.log(`Successfully reloaded application (/) commands.`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+deploy();
